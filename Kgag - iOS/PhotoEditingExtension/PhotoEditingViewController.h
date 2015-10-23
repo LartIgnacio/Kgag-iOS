7475f5dc361721ef828baefbba70f7674f2f2942
LartAceIgnacio <l.ignacio@klab.com>
2015-10-23 13:46:34 +0800

    photo extensions goals.

---
+++ Kgag - iOS/PhotoEditingExtension/PhotoEditingViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoEditingViewController.h
+//  PhotoEditingExtension
+//
+//  Created by Lart Ace Ignacio on 23/10/2015.
+//  Copyright © 2015 L Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoEditingViewController : UIViewController
+
+@end
